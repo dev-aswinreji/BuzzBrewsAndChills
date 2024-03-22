dcec53a2fb42a3b0b732b2a56a75845f1a52c01e
Aswin Reji <aswinreji.004@gmail.com>
2024-03-22 10:36:34 +0530

    made changes in userside and adminside userside verification done need to add error message adminside using multer add new products and showing it on userside

--- src/middleware/multer.mjs
+++ src/middleware/multer.mjs
@@ -1,16 +1,20 @@
 
 import multer from 'multer'
+import path from 'path'
+import { imageDirectory } from '../app.mjs'
 
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        cb(null,'/public/images')
+        cb(null,imageDirectory)
     },
     filename:(req,file,cb)=>{
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()*1E9)
-        cb(null,file.fieldname + '-' + uniqueSuffix)
+        console.log(uniqueSuffix,'middleware file name is showing')
+        cb(null,file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
     }
 })
 
-const upload = multer({storage:storage})
+export const upload = multer({storage:storage})
+
 
 
