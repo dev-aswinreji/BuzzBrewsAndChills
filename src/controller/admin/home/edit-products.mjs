d71fb3a5f623098426073ef7fcb06e656da26335
Aswin Reji <aswinreji.004@gmail.com>
2024-04-24 14:31:51 +0530

    made slight improvemnet in edit-products page pending Cropped image upload and need to unlink oldImage

--- src/controller/admin/home/edit-products.mjs
+++ src/controller/admin/home/edit-products.mjs
@@ -1,5 +1,6 @@
 import { imageDirectory } from "../../../app.mjs"
-import { findCategory, findSingleProduct, findSingleProductWithSameName, } from "../../../data/products/find.mjs"
+import { findCategory, findSingleProduct, findSingleProductWithSameName, findUniqueCategory, } from "../../../data/products/find.mjs"
+import { updateProducts } from "../../../data/products/update.mjs"
 import { checkDataDuplication } from "../../../validation/checking-duplicateData.mjs"
 
 export const admin_editProductsGet = async (req, res) => {
@@ -25,14 +26,27 @@ export const admin_editProductsGet = async (req, res) => {
 export const admin_editProductsPost = async (req, res) => {
 
     try {
+        console.log(req.body);
         const id = req.body.productId
+        const category = await findUniqueCategory(req.body.category)
         const productName = req.body.name.trim()
         const productData = await findSingleProduct(id)
         const isDuplicateProduct = await findSingleProductWithSameName(productName)
         const result = await checkDataDuplication(isDuplicateProduct)
+        const updatedProductData = {
+            name:req.body.name,
+            description:req.body.name,
+            price:req.body.price,
+            category:category,
+            stock:req.body.stock,
+            imageUrl:req.body.old_imageUrl
+
+        }
+        console.log(updatedProductData);
         if(productData.name === productName || result === 'NOT EXIST'){
             try {
-                
+                await updateProducts(id,updatedProductData)
+                res.redirect('/admin/products')
             } catch (error) {
                 console.log(error,'Update Product Error');
             }
