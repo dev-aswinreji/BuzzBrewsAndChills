00d258c7cbcf774d5d577f82b7bcabe1f608a61b
Aswin Reji <aswinreji.004@gmail.com>
2024-04-26 09:50:28 +0530

    used user-auth middleware to check authentication for user in checkout

--- src/route/user/home/user-home.mjs
+++ src/route/user/home/user-home.mjs
@@ -1,36 +1,35 @@
-
 import express from 'express'
 
 const routeHome = express.Router()
 
-import { user_homeGet } from "../../../controller/user/home/home.mjs";
-import { user_addressPost, user_checkoutGet } from '../../../controller/user/home/checkout.mjs';
-import { user_cartGet } from '../../../controller/user/home/cart.mjs';
-import { user_aboutGet } from '../../../controller/user/home/about.mjs';
-import { user_shopGet } from '../../../controller/user/home/shop.mjs';
-import { user_contactGet } from '../../../controller/user/home/contact.mjs';
-import { user_productView } from '../../../controller/user/home/product-view.mjs';
-import { user_profileGet } from '../../../controller/user/home/user-profile.mjs';
-import { user_authentication } from '../../../middleware/user-auth.mjs';
+import {user_homeGet} from "../../../controller/user/home/home.mjs";
+import {user_addressPost, user_checkoutGet} from '../../../controller/user/home/checkout.mjs';
+import {user_cartGet} from '../../../controller/user/home/cart.mjs';
+import {user_aboutGet} from '../../../controller/user/home/about.mjs';
+import {user_shopGet} from '../../../controller/user/home/shop.mjs';
+import {user_contactGet} from '../../../controller/user/home/contact.mjs';
+import {user_productView} from '../../../controller/user/home/product-view.mjs';
+import {user_profileGet} from '../../../controller/user/home/user-profile.mjs';
+import {user_authentication} from '../../../middleware/user-auth.mjs';
 
 routeHome.get('/', user_homeGet)
 routeHome.get('/home', user_homeGet)
 
 routeHome.get('/about', user_aboutGet)
 
-routeHome.get('/contact',user_contactGet)
+routeHome.get('/contact', user_contactGet)
 
-routeHome.get('/shop',user_shopGet)
+routeHome.get('/shop', user_shopGet)
 
-routeHome.get('/contact',user_contactGet)
+routeHome.get('/contact', user_contactGet)
 
-routeHome.get('/user-profile',user_profileGet)
+routeHome.get('/user-profile', user_profileGet)
 
-routeHome.get('/checkout', user_checkoutGet)
+routeHome.get('/checkout', user_authentication, user_checkoutGet)
 routeHome.post('/address', user_addressPost)
 
-routeHome.get('/cart',user_authentication, user_cartGet)
+routeHome.get('/cart', user_authentication, user_cartGet)
 
-routeHome.get('/product-view/:id',user_productView)
+routeHome.get('/product-view/:id', user_productView)
 
 export default routeHome
