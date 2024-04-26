f8eb3ea8f0092919c4139d96a7954544b7dedcd5
Aswin Reji <aswinreji.004@gmail.com>
2024-04-26 19:24:18 +0530

    set new password almost done need to implement using id and update method of mongoose

--- src/controller/user/home/set-new-password.mjs
+++ src/controller/user/home/set-new-password.mjs
@@ -1,10 +1,15 @@
 import { updateUserPassword } from "../../../data/users/update.mjs"
 
 export const user_setNewPasswordGet = async (req,res)=>{
-    res.render('set-new-password')
+    
+    const  userId = req.query.id
+    console.log(userId,'i got the userid using query');
+    res.render('set-new-password',{userId})
 }
 
 export const user_setNewPasswordPost = async (req,res) =>{
+    const userId = req.query.id
+    console.log(userId,'i got user id using post method also');
     const password = req.body.password 
     const email = req.query.email
     await updateUserPassword(email,password)
