a5841cde1332fdfe9fb59d003eebb4b335fb0684
Aswin Reji <aswinreji.004@gmail.com>
2024-04-26 09:42:10 +0530

    used user-auth middleware to check authentication for user

--- src/controller/user/home/cart.mjs
+++ src/controller/user/home/cart.mjs
@@ -1,22 +1,19 @@
 import Swal from "sweetalert2"
-import { findAllProducts } from "../../../data/products/find.mjs"
+import {findAllProducts} from "../../../data/products/find.mjs"
 
 
 export const user_cartGet = async (req, res) => {
 
-  try {
+    try {
 
-    if (req.session.isUserAuth) {
-      const data = req.params.id
-      console.log(data)
-      const cartProduct = await findAllProducts(data)
-      res.render('cartempty', { cartProduct })
-    } else {
-      res.redirect('/signin')
+        const data = req.params.id
+        console.log(data)
+        const cartProduct = await findAllProducts(data)
+        res.render('cartempty', {cartProduct})
+
+
+    } catch (error) {
+        console.error(error)
     }
-
-  } catch (error) {
-    console.error(error)
-  }
 
 }
