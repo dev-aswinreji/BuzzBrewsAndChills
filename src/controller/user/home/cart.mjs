fcdcc5a69a4cd32d2fcd0b4ffdaaf58caacfe708
Aswin Reji <aswinreji.004@gmail.com>
2024-04-28 12:56:48 +0530

    working on user to cart using fetch to update totalPrice need some changes in login page

--- src/controller/user/home/cart.mjs
+++ src/controller/user/home/cart.mjs
@@ -4,7 +4,7 @@ import {findUser, findUserUsingId} from "../../../data/users/find.mjs"
 import {findCartDataDuplicate, findCartDatasOfUser} from "../../../data/cart/find.mjs"
 import {insertCartData} from "../../../data/cart/insert.mjs"
 import {checkDataDuplication} from "../../../validation/checking-duplicateData.mjs"
-import { updateCartDatas } from "../../../data/cart/update.mjs"
+import {updateCartDataOfTotalPrice, updateCartDatas} from "../../../data/cart/update.mjs"
 
 
 export const user_cartGet = async (req, res) => {
@@ -29,16 +29,16 @@ export const user_addToCartGet = async (req, res) => {
         const id = req.query.productId
         const path = req.query.path
         const userEmail = req.session.userEmailForAddUserAddress
-        console.log('useremail is set',userEmail);
-        console.log(id,'product id pakka');
+        console.log('useremail is set', userEmail);
+        console.log(id, 'product id pakka');
         const product = await findSingleProduct(id)
         console.log(product, 'product data is showing ');
         const user = await findUser(userEmail)
-        console.log(user,'user data is showing');
+        console.log(user, 'user data is showing');
         const cartDupicate = await findCartDataDuplicate(user._id, id)
-        console.log(cartDupicate,'cart duplicate');
+        console.log(cartDupicate, 'cart duplicate');
         const result = await checkDataDuplication(cartDupicate)
-        console.log(result,'result of check data dupe');
+        console.log(result, 'result of check data dupe');
         if (result === 'NOT EXIST') {
             const cartData = {
                 userId: user,
@@ -54,15 +54,28 @@ export const user_addToCartGet = async (req, res) => {
             // console.log(cartData.items[0].productId.name);
 
             await insertCartData(cartData)
-            
-        }else{
-            // console.log('update data is working or not ');
-            await updateCartDatas(user._id,id)
+
+        } else { // console.log('update data is working or not ');
+            await updateCartDatas(user._id, id)
         }
         // window.location.reload()
         res.redirect(`/${path}`)
     } catch (error) {
         console.log(error, 'USER ADD TO CART GET');
         res.send(500)
+    }
+}
+
+export const user_addToCartFetchToUpdatingTotalPrice = async (req, res) => {
+    try {
+
+        const id = req.query.productId
+        const userId = req.session.USER_ID
+        const totalPrice = req.query.totalPrice
+
+        await updateCartDataOfTotalPrice(userId,id,totalPrice)
+
+    } catch (error) {
+        console.log(error, 'USER ADD TO CART FETCH TO UPDATE TOTAL PRICE');
     }
 }
--- src/data/cart/update.mjs
+++ src/data/cart/update.mjs
@@ -5,3 +5,8 @@ export async function updateCartDatas (id,productId){
     const data = await cartCollection.updateOne({userId:id,"items.productId":productId},{$inc:{'items.$.quantity':1,totalPrice:1}})
     console.log(data,'updated data is showing');
 }
+
+export async function updateCartDataOfTotalPrice (id,productId,totalPrice){
+    const cartData = await cartCollection.updateOne({userId:id,"items.productId":productId},{$addToSet:{totalPrice:totalPrice}})
+    console.log(cartData,'updated cart data price is showing');
+}
