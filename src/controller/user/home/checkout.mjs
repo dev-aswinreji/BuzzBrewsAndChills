ef674c18c3d02e3fa174376d062815796f5dc04a
Aswin Reji <aswinreji.004@gmail.com>
2024-05-04 00:29:31 +0530

    make small improvement rendered detail in checkout page

--- src/controller/user/home/checkout.mjs
+++ src/controller/user/home/checkout.mjs
@@ -1,19 +1,20 @@
 import Swal from "sweetalert2";
-import {findUser, findUserAddressUsingPopulate} from "../../../data/users/find.mjs";
+import {findDefaultUserAddressUsingPopulate, findUser, findUserAddressUsingPopulate} from "../../../data/users/find.mjs";
 import {insertUserAddress} from "../../../data/users/insert.mjs";
 import {updateUser} from "../../../data/users/update.mjs";
+import { findAllCartDatas } from "../../../data/cart/find.mjs";
 
 
 export const user_checkoutGet = async (req, res) => {
 
     try {
 
-
-        const userEmail = req.session.userEmailForAddUserAddress
-
-        const user = await findUserAddressUsingPopulate(userEmail)
-        console.log(user)
-        res.render('checkout', {user})
+        const userId = req.session.USER_ID
+        const cartDatas = await findAllCartDatas()
+        console.log(cartDatas,'cart data is showing');
+        const defaultAddress = await findDefaultUserAddressUsingPopulate(userId)
+        console.log(defaultAddress)
+        res.render('checkout', {defaultAddress,cartDatas})
 
     } catch (error) {
         console.error(error, 'USER CHECKOUT GET')
