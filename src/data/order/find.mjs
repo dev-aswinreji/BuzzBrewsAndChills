73d239f6d597c3c18377300c86249b8aede33d95
Aswin Reji <aswinreji.004@gmail.com>
2024-05-04 13:49:13 +0530

    order sorted by date latest order come first

--- src/data/order/find.mjs
+++ src/data/order/find.mjs
@@ -2,5 +2,5 @@ import { orderCollection } from "../../model/order.mjs";
 
 
 export async function findOrderData (userId){
-    return await orderCollection.find({userId:userId}).populate('items.productId')
+    return await orderCollection.find({userId:userId}).populate('items.productId').sort({createdAt:-1})
 }
