810476756c3da27035db6e68369f7c55663e683e
Aswin Reji <aswinreji.004@gmail.com>
2024-05-04 12:07:36 +0530

    made improvements product view page and add stock limit in add to cart controller

--- src/controller/user/home/add-to-cart.mjs
+++ src/controller/user/home/add-to-cart.mjs
@@ -9,11 +9,13 @@ export const user_addToCartGet = async (req, res) => {
     const userId = req.session.USER_ID
     const productId = req.query.productId
     const quantity = req.query.quantity
+    const stock = req.query.stock
+    console.log(stock);
     console.log(quantity,'quantity of product as query');
     const product = await findSingleProduct(productId)
-
-    const cart = await findDuplicateCartProducts(userId, product)
-
+    console.log(product,'product data is showing');
+    const cart = await findDuplicateCartProducts(userId)
+    console.log(cart,'cart data is showing ===================================');
     const result = await checkDataDuplication(cart)
 
 
@@ -21,10 +23,13 @@ export const user_addToCartGet = async (req, res) => {
         await addToCartData(userId, product)
         return res.json({result: 'within limit'})
     } else {
-        const cartItem = cart.items.find(item => item.productId.toString() === productId.toString())
-        const cartQuantityOfSpecificProduct = cartItem.quantity
-        console.log(cartQuantityOfSpecificProduct, 'quantity of each product is');
-        const response = cartQuantityOfSpecificProduct < 10 ? await addToCartDataSameProductUpdate(userId, product, quantity)
+        const cartItem = cart.items.filter(item => item.productId._id.toString() === productId.toString())
+        console.log(cartItem,'what is inside cartItem=======================================l;fjglk;sd======================');
+        const quantity = cartItem[0].quantity
+        const stock = cartItem[0].productId.stock
+        console.log(stock,'stock is showing');
+        console.log(quantity, 'quantity of each product is');
+        const response = stock > 0 && quantity < 10 ? await addToCartDataSameProductUpdate(userId, product, quantity)
         .then(() => ({result: 'within limit'})) : ({result: 'limit exceeded'})
         console.log(response, 'response is showing');
         return res.json(response)
--- src/data/cart/find.mjs
+++ src/data/cart/find.mjs
@@ -4,8 +4,8 @@ export async function findAllCartDatas (){
     return await cartCollection.findOne().populate('items.productId')
 }
 
-export async function findDuplicateCartProducts(userId,product){
-    return await cartCollection.findOne({userId:userId,"items.productId":product})
+export async function findDuplicateCartProducts(userId){
+    return await cartCollection.findOne({userId:userId}).populate('items.productId')
 }
 
 
