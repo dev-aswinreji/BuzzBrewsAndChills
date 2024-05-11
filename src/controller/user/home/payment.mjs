4c92e3a92b2bfed0bdb477fe31b7ac0a244dc18a
dev-aswinreji <aswinreji.004@gmail.com>
2024-05-11 11:04:16 +0530

    made small improvement in payment page added razorpay need to make more updation

--- src/controller/user/home/payment.mjs
+++ src/controller/user/home/payment.mjs
@@ -1,5 +1,7 @@
 
 export const user_paymentGet = async (req,res)=>{
     req.session.USER_ORDER_DEFAULT_ADDRESSID = req.query.addressId
-    res.render('payment')
+    const amount = req.query.totalPrice
+    console.log(amount);
+    res.render('payment',{amount})
 }
--- src/controller/user/home/razorpay.mjs
+++ src/controller/user/home/razorpay.mjs
@@ -1,8 +1,17 @@
-import { razorpayInstance } from "../../../utils/razorpay.mjs"
+import { razorpayInstance } from "../../../utils/razorpay.mjs";
 
-export const user_razorpay = async (req,res)=>{
-    const {amount} = req.body
-    const currency = 'INR'
-    const order = await razorpayInstance.orders.create({amount,currency})
-    console.log(order,'order is showing');
-}
+export const user_razorpay = async (req, res) => {
+  try {
+    const amount = req.query.amount * 100
+
+    console.log(amount,'wht is happening');
+    const currency = "INR";
+    const receipt = "ORD_1234567890"; // Example receipt ID
+    const notes = { description: "New order", customer: "John Doe" };
+    const order = await razorpayInstance.orders.create({ amount, currency,receipt,notes });
+    console.log(order, "order is showing");
+    
+  } catch (error) {
+    console.log(error, "USER RAZORPAY GET");
+  }
+};
