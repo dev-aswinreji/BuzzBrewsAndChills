d86f4b20960a7469165ed14157136f518dc32177
dev-aswinreji <aswinreji.004@gmail.com>
2024-05-15 20:00:14 +0530

    made some changes in order schema

--- src/model/order.mjs
+++ src/model/order.mjs
@@ -1,72 +1,51 @@
+import moment from "moment";
+import mongoose, { Schema } from "mongoose";
 
-
-
-import moment from "moment"
-import mongoose, { Schema } from "mongoose"
-
-const db = await import("./database.mjs").then((instance) => instance.default)
+const db = await import("./database.mjs").then((instance) => instance.default);
 
 function cartModel(collectionName, schema) {
-    return db.model(collectionName, schema)
+  return db.model(collectionName, schema);
 }
 
-
-
 const orderSchema = mongoose.Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'userData',
-        required:true
-    },
-    orderId:{
-        type:String,
-        required:true
-    },
-    items: [{
-        productId: {
-            type: Schema.Types.ObjectId,
-            ref: 'products',
-            required:true
-        },
-        quantity:{
-            type:Number,
-            min:1,
-            required:true
-        },
-        price:{
-            type:Number,
-            required:true
-        }
-    }],
-    totalPrice:{
-        type:Number,
-        min:0,
-        required:true,
-    },
-    status:{
-        type:String,
-        enum:['PROCESSING','OUT FOR DELIHVERY','DELIVERED'],
-        default: 'PROCESSING'
-    },
-    paymentMethod:{
-        type:String,
-        required:true,
-    },
-    productStatus:{
-        type:String,
-        enum :['ACTIVE','CANCELLED'],
-        default:'ACTIVE'
-    },
-    orderDate:{
-        type: String,
-        default: moment().format('YYYY/MM/DD')
-    },
-    paymentId:{
-        type:String,
-        
-    }
+  userId: {
+    type:String,
+    trim:true,
+    required: true,
+  },
+  orderId: {
+    type: String,
+    required: true,
+  },
+  products: {
+    type:Array,
+    required: true,
+  },
+  address:{
+    type:String,
+    required:true,  
+  },
+  totalPrice: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["PROCESSING", "OUT FOR DELIHVERY", "DELIVERED","CANCELLED"],
+    default: "PROCESSING",
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  orderDate: {
+    type: String,
+    default: moment().format("YYYY/MM/DD"),
+  },
+  onlinePaymentId: {
+    type: String,
+  },
+});
 
-})
-
-
-export const orderCollection = cartModel('order',orderSchema)
+export const orderCollection = cartModel("order", orderSchema);
