e7b2435a84ec8ee75f5055270beafdd05d53be50
dev-aswinreji <aswinreji.004@gmail.com>
2024-05-24 19:47:23 +0530

    added coupon page in adminside edit and create

---
+++ src/controller/admin/home/coupon.mjs
@@ -0,0 +1,9 @@
+
+
+export const admin_couponGet = async (req,res)=>{
+    res.render('coupon')
+}
+
+export const admin_couponPost = async (req,res)=>{
+    
+}
---
+++ src/controller/admin/home/edit-coupon.mjs
@@ -0,0 +1,9 @@
+
+
+export const admin_editCouponGet = async (req,res)=>{
+    res.render('edit-coupon')
+}
+
+export const admin_editCouponPost = async (req,res)=>{
+    
+}
