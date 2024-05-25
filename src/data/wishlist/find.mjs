f266913cc33c948bfd577d7aeae95fe3327cf811
dev-aswinreji <aswinreji.004@gmail.com>
2024-05-25 18:59:17 +0530

    fixed duplicate product adding on wishlist

--- src/data/wishlist/find.mjs
+++ src/data/wishlist/find.mjs
@@ -4,3 +4,7 @@ import { wishlistCollection } from "../../model/wishlist.mjs";
 export async function findWishlistProductForUser (userId){
     return await wishlistCollection.findOne({userId:userId}).populate('products.productId')
 }
+
+export async function findDuplicateWishlistProductOfUser (userId,productId){
+    return await wishlistCollection.findOne({userId:userId,"products.productId":productId})
+}
