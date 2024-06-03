1b92b4236c4c62edad049226873988baf7aad651
dev-aswinreji <aswinreji.004@gmail.com>
2024-06-03 21:05:09 +0530

    add couponDiscount as 0 if there is no coupon discount applied

--- src/controller/user/home/add-order.mjs
+++ src/controller/user/home/add-order.mjs
@@ -8,7 +8,7 @@ export const user_orderAddGet = async (req, res) => {
     const paymentMethod = req.query.paymentMethod;
     const paymentId = req.query.paymentId
     const address = req.query.address
-    const couponDiscount = req.query.couponDiscount
+    const couponDiscount = req.query.couponDiscount || 0
     console.log(paymentMethod,'payment method');
     console.log(paymentId,'payment id is showing');
     console.log(address,'address is showing');
