4dd3d45c9d63d547df185a57c0cb854c4fab8dc6
dev-aswinreji <aswinreji.004@gmail.com>
2024-06-13 18:37:10 +0530

    using trycatch in coupon page

--- src/controller/admin/home/edit-coupon.mjs
+++ src/controller/admin/home/edit-coupon.mjs
@@ -3,44 +3,62 @@ import { adminCouponStatusUpdate, adminCouponUpdate } from "../../../data/coupon
 import { checkDataDuplication } from "../../../validation/checking-duplicateData.mjs"
 
 export const admin_editCouponGet = async (req, res) => {
-    const { couponId } = req.query
-    const coupon = await findUniqueCouponForAdminToEdit(couponId)
-    console.log(coupon);
-    res.render('edit-coupon', { coupon })
+
+    try {
+        const { couponId } = req.query
+        const coupon = await findUniqueCouponForAdminToEdit(couponId)
+        console.log(coupon);
+        res.render('edit-coupon', { coupon })
+    } catch (error) {
+        console.log(error, 'EDIT COUPON GET ERROR');
+    }
+
 }
 
 export const admin_updateCouponStatusPut = async (req, res) => {
-    const { couponId, couponStatus } = req.body
-    await adminCouponStatusUpdate(couponId, couponStatus)
-    res.json({ result: 'success' })
+    try {
+
+        const { couponId, couponStatus } = req.body
+        await adminCouponStatusUpdate(couponId, couponStatus)
+        res.json({ result: 'success' })
+
+    } catch (error) {
+        console.log(error, 'ADMIN UPDATE COUPON STATUS PUT');
+    }
 }
 
 
 export const admin_editCouponPost = async (req, res) => {
 
-    const { couponId } = req.query
-    const formData = req.body
-    const {
-        name, discount, starting_date, expiry, minimum_cart_price, description
-    } = formData
+    try {
+        const { couponId } = req.query
+        const formData = req.body
+        const {
+            name, discount, starting_date, expiry, minimum_cart_price, description
+        } = formData
 
-    const updatedCouponData = {
-        name: name,
-        discount: Number(discount),
-        starting_date: starting_date,
-        ending_date: expiry,
-        mininmum_cart_price: Number(minimum_cart_price),
-        description: description,
-    }
-    const coupon = await findUniqueCouponForAdmin(updatedCouponData.name)
-    const result = await checkDataDuplication(coupon)
+        const updatedCouponData = {
+            name: name,
+            discount: Number(discount),
+            starting_date: starting_date,
+            ending_date: expiry,
+            mininmum_cart_price: Number(minimum_cart_price),
+            description: description,
+        }
+        const coupon = await findUniqueCouponForAdmin(updatedCouponData.name)
+        const result = await checkDataDuplication(coupon)
 
-    if (result === 'NOT EXIST' || coupon._id.equals(couponId)) {
-        await adminCouponUpdate(couponId, updatedCouponData)
-        res.json({ result: 'success' })
-    } else {
-        req.session.couponError = 'coupon already exists'
-        res.json({ result: 'already exists' })
+        if (result === 'NOT EXIST' || coupon._id.equals(couponId)) {
+            await adminCouponUpdate(couponId, updatedCouponData)
+            res.json({ result: 'success' })
+        } else {
+            req.session.couponError = 'coupon already exists'
+            res.json({ result: 'already exists' })
+        }
+    } catch (error) {
+        console.log(error, 'ADMIN EDIT COUPON POST ERROR');
     }
+
+
 
 }
