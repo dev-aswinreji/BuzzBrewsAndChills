deeee9f1d66a989859bb4114ef8e4e33ef89e81b
dev-aswinreji <aswinreji.004@gmail.com>
2024-06-23 15:29:28 +0530

    feat:  order placed email confirmation

--- src/controller/user/order/order-placed.mjs
+++ src/controller/user/order/order-placed.mjs
@@ -1,9 +1,14 @@
+import { findUserUsingId } from "../../../data/users/find.mjs";
+import { sendEmailForOrderPlaced } from "../../../utils/sendMail-orderPlaced.mjs";
 
 
 export const user_orderPlacedGet = async (req,res)=>{
     try {
         const order = req.session.ORDER_PLACED
         // console.log(order[0].products,'order is showing in orderplaced page');   
+        const userId = req.session.USER_ID
+        const user = await findUserUsingId(userId)
+        sendEmailForOrderPlaced(user,order)
         res.render('order-placed',{order})
         // req.session.ORDER_PLACED = false
         console.log('line of code is showing after render');
