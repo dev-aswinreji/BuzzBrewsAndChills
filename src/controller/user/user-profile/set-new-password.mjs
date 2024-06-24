d436aee29a5adff5c09d7cf8cc4c7cf6c224117f
dev-aswinreji <aswinreji.004@gmail.com>
2024-06-24 17:43:54 +0530

    feat: set new password new feature added need to make it as fetch confirmation checking pass is true or false

--- src/controller/user/user-profile/set-new-password.mjs
+++ src/controller/user/user-profile/set-new-password.mjs
@@ -6,29 +6,28 @@ export const user_setNewPasswordGet = async (req, res) => {
 
     const userId = req.query.id
     console.log(userId, 'i got the userid using query');
-    const error = req.session.setNewPasswordError 
-    res.render('set-new-password', { userId ,error})
+    const error = req.session.setNewPasswordError
+    res.render('set-new-password', { userId, error })
 }
 
 export const user_setNewPasswordPost = async (req, res) => {
     const userId = req.session.USER_ID
     const user = await findUserUsingId(userId)
-    console.log(user,'user is showing');
+    console.log(user, 'user is showing');
     console.log(req.body, 'req.body is showing');
     console.log(req.query, 'hehe');
 
-    
+
     if (req.query.oldPassword) {
-        const oldPassword = await compareHashPassword(req.query.oldPassword,user.password)
-        return res.json({ result: 'success' ,oldPassword})
-    } else if(req.body.password){
+        const oldPassword = await compareHashPassword(req.query.oldPassword, user.password)
+        return res.json({ result: 'success', oldPassword })
+    } else if (req.query.comparingPass) {
 
         console.log(userId, 'i got user id using post method also');
-        const comparingOldAndNewPass = await compareHashPassword(req.body.password,user.password)
-        if(comparingOldAndNewPass === true){
-            req.session.setNewPasswordError = 'old pass and new pass is same'
-            return res.redirect("/set-new-password")
-        }
+        const comparingOldAndNewPass = await compareHashPassword(req.query.comparingPass, user.password)
+        return res.json({ result: "same pass", comparingOldAndNewPass })
+
+    } else if (req.body.password) {
         const newPasswordpassword = req.body.password
         const password = await hashPassword(newPasswordpassword)
         console.log(password);
