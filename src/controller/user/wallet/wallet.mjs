a1b1433805e48685d78dca8b24b5acbc3d1084d1
dev-aswinreji <aswinreji.004@gmail.com>
2024-06-25 09:35:23 +0530

    feat: pagination

--- src/controller/user/wallet/wallet.mjs
+++ src/controller/user/wallet/wallet.mjs
@@ -1,14 +1,29 @@
 import { findWalletAmount } from "../../../data/wallet/find.mjs"
 
 
-export const user_walletGet = async (req,res)=>{
+export const user_walletGet = async (req, res) => {
     try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = 6; // Number of transactions per page
+
         const userId = req.session.USER_ID
         const wallet = await findWalletAmount(userId)
-        console.log(wallet,'wallet is showing');
-        res.render('wallet',{wallet})
+       
+        const totalTransactions = wallet.walletTransactions.length;
+        const totalPages = Math.ceil(totalTransactions / limit);
+
+        const transactions = wallet.walletTransactions
+            .sort((a, b) => new Date(b.date) - new Date(a.date)) // Sort by date descending
+            .slice((page - 1) * limit, page * limit); // Paginate
+
+        res.render('wallet', {
+            wallet,
+            transactions,
+            currentPage: parseInt(page),
+            totalPages
+        });
     } catch (error) {
-        console.log(error,'USER WALLET GET ');
+        console.log(error, 'USER WALLET GET ');
     }
-  
+
 }
