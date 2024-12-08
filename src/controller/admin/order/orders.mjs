2d5494014f30d38e1f79aa6d11854b628b2e9356
Aswin Reji <aswinreji.004@gmail.com>
2024-12-08 15:10:29 +0000

    fix: removed some unwanted line of code

--- src/controller/admin/order/orders.mjs
+++ src/controller/admin/order/orders.mjs
@@ -8,14 +8,11 @@ export const admin_ordersGet = async (req, res) => {
         
     const TOTAL_COUNT_OF_ORDERS = await findTotalCountOfAllOrdersForAdmin();
     const totalPages = Math.ceil(TOTAL_COUNT_OF_ORDERS / limit);
-    console.log(TOTAL_COUNT_OF_ORDERS,'total count is showing');
     page = Math.max(1, Math.min(page, totalPages));
 
     const skip = (page - 1) * limit;
    
     const orders = await findAllOrderDataForAdmin(skip,limit)
-    console.log(orders,'orders is showing');
-    console.log(orders[0]?.products);
     res.render("orders",{orders, page,totalPages, count: TOTAL_COUNT_OF_ORDERS});
   } catch (error) {
     console.error(error, " ADMIN ORDERS GET ");
