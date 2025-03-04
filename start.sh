7752a10a790c24ee133e572a36264a892f197b7b
Aswin Reji <aswinreji.004@gmail.com>
2025-03-04 23:20:28 +0000

    is it working properly using script need to check added bash shebang and added sudo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # removed shebang
 #!/bin/bash
 if [ ! -d "public/product-images" ]; then
-  mkdir -p public/product-images
+  sudo mkdir -p public/product-images
 fi
 nodemon --env-file=.env src/app.mjs
